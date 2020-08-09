2e0da37d1fd0ff06d0d1f49ec9a8cccc2462c32b
haseeb1361 <haseebahmed1361@gmail.com>
2020-08-09 11:32:16 -0700

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,87 @@
+function check(){
+    var score = 0
+    var right_answer_1 = document.getElementById('q1-a1');
+    var q_answer_2 = document.getElementById("q1-a2");
+    var q1_answer_3 = document.getElementById("q1-a3");
+    var q1_answer_4 = document.getElementById("q1-a4");
+    if (right_answer_1.checked == true){
+    score++
+    alert('Q1 answer right')
+    } 
+else{
+    alert('q1 is wrong')
+}
+
+
+
+
+
+
+    var right_answer_2 = document.getElementById('q2-a1');
+    var q2_answer_2 = document.getElementById("q2-a2");
+    var q2_answer_3 = document.getElementById("q2-a3");
+    var q2_answer_4 = document.getElementById("q2-a4");
+    if (right_answer_2.checked == true){
+        score++
+        alert('Q2 answer right')
+        } 
+    else{
+        alert('q2 is wrong')
+    }
+
+
+
+
+
+    
+    var right_answer_3 = document.getElementById('q3-a1');
+    var q3_answer_2 = document.getElementById("q3-a2");
+    var q3_answer_3 = document.getElementById("q3-a3");
+    var q3_answer_4 = document.getElementById("q3-a4");
+    if (right_answer_3.checked == true){
+        score++
+        alert('Q3 answer right')
+        } 
+    else{
+        alert('q3 is wrong')
+    }
+
+
+
+
+
+
+
+    var right_answer_3 = document.getElementById('q4-a1');
+    var q4_answer_2 = document.getElementById("q4-a2");
+    var q4_answer_3 = document.getElementById("q4-a3");
+    var q4_answer_4 = document.getElementById("q4-a4");
+    if (right_answer_3.checked == true){
+        score++
+        alert('Q4 answer right')
+        } 
+    else{
+        alert('q4 is wrong')
+    }
+
+
+
+
+
+
+
+
+    var right_answer_5 = document.getElementById('q5-a1');
+    var q5_answer_2 = document.getElementById("q5-a2");
+    var q5_answer_3 = document.getElementById("q5-a3");
+    var q5_answer_4 = document.getElementById("q5-a4");
+    if (right_answer_5.checked == true){
+        score++
+        alert('Q5 answer right')
+        } 
+    else{
+        alert('q5 is wrong')
+    }
+    alert("Your score is" +" "+ score++)
+    }
+
